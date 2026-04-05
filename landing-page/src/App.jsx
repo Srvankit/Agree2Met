960c733bbbea57a8c381d56db26485f0e4b97737
Ayush Kumar <ayush.kccitm@gmail.com>
2026-04-06 02:41:02 +0530

    Updated app.jsx (#6)

--- landing-page/src/App.jsx
+++ landing-page/src/App.jsx
@@ -3,9 +3,17 @@ import React from 'react'
 
 const App = () => {
   return (
+
+    <div>App</div>
+  )
+}
+
+export default App
+=======
     <div>Hiiii</div>
   )
 }
 
 export default App
 
+>>>>>>> 6359aa4c034f6e800bbfee004d69fa92d0f50215
