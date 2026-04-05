f33f3a011f3386a06d27bc472515aa7c113d1351
ankityadav.reh@gmail.com <167362173+Srvankit@users.noreply.github.com>
2026-04-06 02:46:13 +0530

    Added boiler plate code for app.css

--- landing-page/src/App.jsx
+++ landing-page/src/App.jsx
@@ -1,19 +1,8 @@
-
 import React from 'react'
-
 const App = () => {
   return (
 
-    <div>App</div>
+    <div>Home</div>
   )
 }
-
 export default App
-=======
-    <div>Hiiii</div>
-  )
-}
-
-export default App
-
->>>>>>> 6359aa4c034f6e800bbfee004d69fa92d0f50215
