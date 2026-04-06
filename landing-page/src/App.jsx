700a650e4a190785bcee9e30626198f513754337
Om Singh <omsingh1.tech&gmail.com>
2026-04-07 00:00:36 +0530

    Change updated

--- landing-page/src/App.jsx
+++ landing-page/src/App.jsx
@@ -1,11 +1,35 @@
+import React from "react";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
 
-import React from 'react'
+// Pages
+import Home from "./pages/Home";
+
+// Sections
+import Features from "./components/Features/Features";
+import HowItWorks from "./components/HowItWorks/HowItWorks";
 
 const App = () => {
   return (
-    <div>Hiiii</div>
-  )
-}
+    <>
+      <Navbar />
 
-export default App
+      <main style={{ minHeight: "80vh" }}>
 
+        {/* 🔥 HERO SECTION */}
+        <Home />
+
+        {/* 🚀 FEATURES SECTION */}
+        <Features />
+
+        {/* ⚙️ HOW IT WORKS SECTION */}
+        <HowItWorks />
+
+      </main>
+
+      <Footer />
+    </>
+  );
+};
+
+export default App;
