13c514672beb309b5afb2ea7ab762a466065ca4c
Ayush Kumar <ayush.kccitm@gmail.com>
2026-04-06 23:07:49 +0530

    Ayush branch (#8)
    * Updated app.jsx
    
    * updated navabar and footer

--- landing-page/src/App.jsx
+++ landing-page/src/App.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+
 const App = () => {
   return (
+    <>
+      <Navbar />
 
-    <div>Home</div>
-  )
-}
-export default App
+      <main style={{ minHeight: "80vh" }}>
+        <h1 style={{ textAlign: "center", marginTop: "50px" }}>
+          Welcome to AutoTrust
+        </h1>
+      </main>
+
+      <Footer />
+    </>
+  );
+};
+
+export default App;
