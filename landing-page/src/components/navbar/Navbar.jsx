120be0e1582dcf5f064a8d62d84c23b04de7e43c
Ayush <ayush.kccitm@gmail.com>
2026-04-09 12:01:00 +0530

    added dashboard folder

--- landing-page/src/components/navbar/Navbar.jsx
+++ landing-page/src/components/navbar/Navbar.jsx
@@ -1,55 +1,3 @@
-<<<<<<< HEAD
-import React, { useState } from "react";
-import "./navbar.css";
-
-const Navbar = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false); // change manually to test
-
-  return (
-    <nav className="navbar">
-      <div className="navbar-container">
-
-        {/* LEFT: Logo */}
-        <div className="navbar-logo">
-          <span className="logo-icon">⟡</span>
-          <span className="logo-text">AutoTrust</span>
-        </div>
-
-        {/* CENTER: Navigation */}
-        <div className="navbar-links">
-          <a className="nav-link active">Dashboard</a>
-          <a className="nav-link">Agreements</a>
-          <a className="nav-link">Templates</a>
-          <a className="nav-link">Team</a>
-        </div>
-
-        {/* RIGHT: Auth / Profile */}
-        <div className="navbar-actions">
-
-          {!isLoggedIn ? (
-            <>
-              <button className="login-btn">Login</button>
-              <button className="signup-btn">Sign Up</button>
-            </>
-          ) : (
-            <>
-              <span className="bell">🔔</span>
-              <img
-                src="https://i.pravatar.cc/40"
-                alt="profile"
-                className="profile-img"
-              />
-            </>
-          )}
-        </div>
-
-      </div>
-    </nav>
-  );
-};
-
-export default Navbar;
-=======
 import { useEffect, useState } from "react";
 import "./navbar.css";
 
@@ -139,4 +87,4 @@ export default function Navbar() {
     </nav>
   );
 }
->>>>>>> d6723f47b0d52037b1f906b0c2a0b55ff9f30677
+
