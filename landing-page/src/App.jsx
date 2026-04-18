ee6b5f14c85b7db034ce5ea1b97726e3cabe6861
Ayush <ayush.kccitm@gmail.com>
2026-04-18 12:42:22 +0530

    fixed

--- landing-page/src/App.jsx
+++ landing-page/src/App.jsx
@@ -6,22 +6,19 @@ import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 import AuthModal from "./components/Auth/AuthModel";
 
-<<<<<<< HEAD
-// Sections (correct paths)
-=======
+
 // Sections
->>>>>>> 941c32dfebc0a2c4360c5eecdc15d1c758d84782
+
 import Hero from "./sections/hero/Hero";
 import Features from "./sections/features/Features";
 import HowItWorks from "./sections/howItWorks/HowItWorks";
 import Preview from "./sections/preview/Preview";
 import Testimonials from "./sections/testimonials/Testimonials";
 
-<<<<<<< HEAD
-=======
+
 // Dashboard
 import DashboardLayout from "./dashboard/dashboardLayout/DashboardLayout";
->>>>>>> 941c32dfebc0a2c4360c5eecdc15d1c758d84782
+
 
 const App = () => {
   const [authOpen, setAuthOpen] = useState(false);
@@ -31,17 +28,14 @@ const App = () => {
       {/* ✅ ROUTES */}
       <Routes>
 
-<<<<<<< HEAD
 
-      <main>
-=======
         {/* 🌐 LANDING PAGE */}
         <Route
           path="/"
           element={
             <>
               <Navbar openAuth={() => setAuthOpen(true)} /> {/* 🔥 IMPORTANT */}
->>>>>>> 941c32dfebc0a2c4360c5eecdc15d1c758d84782
+
 
               <main>
                 <Hero />
@@ -61,24 +55,12 @@ const App = () => {
 
       </Routes>
 
-<<<<<<< HEAD
-        {/* 👀 PREVIEW */}
-        <Preview />
 
-        {/* 💬 TESTIMONIALS */}
-        <Testimonials />
-
-
-      </main>
-
-      <Footer />
-=======
       {/* ✅ GLOBAL AUTH MODAL */}
       <AuthModal
         isOpen={authOpen}
         onClose={() => setAuthOpen(false)}
       />
->>>>>>> 941c32dfebc0a2c4360c5eecdc15d1c758d84782
     </>
   );
 };
